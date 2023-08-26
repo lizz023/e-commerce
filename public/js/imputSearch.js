08922616444adeb32eec0a2cbe11e0fc68e87d47
Lizz023 <eliiza.023@gmail.com>
2023-08-25 23:19:13 -0500

    Agrego nuevo input de busqueda para mobile

--- public/js/imputSearch.js
+++ public/js/imputSearch.js
@@ -13,14 +13,24 @@ import { productsGenericService } from "../services/products-service.js";
 const product = document.getElementById("search");
 
 product.addEventListener("submit", (e) => {
-    e.preventDefault()
-    const userInput = document.getElementById("searchInput").value;
+    e.preventDefault();
 
-    const listProducts = productsGenericService.products();
+    const searchResultsList = document.getElementsByClassName('js-search-results')[0],
+        userInput = document.getElementById("searchInput").value,
+        listProducts = productsGenericService.products();
 
     listProducts.then( search => {
         const productsFiltered = search.filter(product => product.name.toLowerCase().includes(userInput.toLowerCase()))
-        console.log(productsFiltered);
+        console.clear();
+        const resultsList = document.createElement('ul');
+
+        productsFiltered.forEach(product => {
+            resultsList.innerHTML += `<li class="navbar__li"><a href="${product.id}" class="icon-Icone search-results__link">${product.name}</a></li>`   
+        });
+
+        searchResultsList.innerHTML = '';
+        searchResultsList.append(resultsList);
+        searchResultsList.classList.remove('hidden');
     });
   
 })
